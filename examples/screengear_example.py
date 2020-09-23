ad93aa47b108a6144196e75801eb78adef17de6a
Hovakim Grabski <hovakim_grabski@yahoo.com>
2020-09-23 21:22:33 +0400

    screengear example worls

---
+++ examples/screengear_example.py
@@ -0,0 +1,35 @@
+# import required libraries
+from vidgear.gears import ScreenGear
+import cv2
+
+# open video stream with default parameters
+stream = ScreenGear().start()
+
+# loop over
+while True:
+
+    # read frames from stream
+    frame = stream.read()
+
+    # check for frame if Nonetype
+    if frame is None:
+        break
+
+
+    # {do something with the frame here}
+
+
+    # Show output window
+    cv2.imshow("Output Frame", frame)
+
+    # check for 'q' key if pressed
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+# close output window
+cv2.destroyAllWindows()
+
+# safely close video stream
+stream.stop()
+
